e60608a95fb7901dceb3975babf05a06f1bfe1d3
vscesar <136080864+vscesar@users.noreply.github.com>
2024-11-14 09:50:29 -0300

    Add files via upload

---
+++ tp programación/script.js
@@ -0,0 +1,14 @@
+// Función para enviar pregunta al servidor y mostrar respuesta
+//function enviarPregunta() {
+ // const pregunta = document.getElementById('pregunta').value;
+  //const xhr = new XMLHttpRequest();
+  //xhr.open('POST', 'enviar-pregunta.php', true);
+  //xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  //xhr.send(`pregunta=${pregunta}`);
+  //xhr.onload = function() {
+    //  if (xhr.status === 200) {
+      //    const respuesta = xhr.responseText;
+        //  const respuestas = document.getElementById('respuestas');
+         // const nuevaRespuesta = document.createElement('li');
+          //nuevaRespuesta.textContent = respuesta;
+          //respuestas.appendChild(n)
